433a5b1b386483125eaec27eda5119ae3d5dbc5c
Craig Morton <CraigMorton92@gmail.com>
2016-03-29 00:48:14 +0100

    works fairly well - code spaghetti - too late to tidy up tonight

--- public/app.js
+++ public/app.js
@@ -11,7 +11,7 @@ var quote4 = new Quote({text: "Scaffolding is nothing but a fiery hell.", author
 quotes = [quote1, quote2, quote3, quote4];
 
 var domLoaded = function(){
-  //
+  // select page elements and save to variables
   var quotesSection = document.querySelector("#quotes");
   var newQuoteSection = document.querySelector("#newQuoteSection");
   var newQuoteButton = document.querySelector(".newQuoteButton");
@@ -51,7 +51,7 @@ var domLoaded = function(){
       }//.bind(this)
       return deleteThisQuote;
     }
-    
+
     for (var i = 0; i < blockQuotesCollection.length; i++){
       deleteButtonsCollection[i].onclick = makeDeleteQuoteFunction(i);
     }
@@ -74,6 +74,7 @@ var domLoaded = function(){
 var addQuoteForm = document.createElement("form");
 addQuoteForm.classList.add("add-quote");
 var addQuoteText = document.createElement("input");
+addQuoteText.autofocus = true;
 addQuoteText.type = "text";
 var addQuoteAuthor = document.createElement("input");
 addQuoteAuthor.type = "text";
@@ -94,6 +95,38 @@ var createAddQuoteForm = function(){
   addQuoteForm.appendChild(addQuoteSubmit);
   newQuoteSection.appendChild(addQuoteForm);
   addQuoteForm.style.display = "none";
+
+  // #### SECOND PLACE I TRIED PUTTING THE DYNAMIC PREVIEW
+
+
+  // var quoteObject = {
+  //   text: addQuoteText.value,
+  //   author: addQuoteAuthor.value
+  // }
+  // var quotesSection = document.querySelector("#quotes")
+  // quoteArticleLive = document.createElement("article");
+  // // deleteButtonLive = document.createElement("button");
+  // // deleteButtonLive.id = "deleteButton";
+  // // deleteButtonLive.innerText = "- delete -";
+  // quoteArticleLive.classList.add("quote");
+  // blockQuoteLive = document.createElement("blockquote");
+  // blockQuoteLive.classList.add("blockquote-live");
+  // // blockQuoteLive.innerHTML = quoteObject.text;
+  // // citeLive = document.createElement("cite");
+  // // citeLive.classList.add("cite-live");
+  // // blockQuoteLive.appendChild(deleteButtonLive);
+  // // citeLive.innerHTML = "- " + quoteObject.author;
+
+  // // citeLive = document.createElement("cite");
+  // // citeLive.classList.add("cite-live")
+  // quotesSection.appendChild(quoteArticleLive);
+  // quoteArticleLive.appendChild(blockQuoteLive);
+  // // blockQuoteLive.appendChild(citeLive);
+  // // citeLive.innerHTML = "fgh";
+
+  // // blockQuoteLive.appendChild(citeLive);
+  // // citeLive.innerHTML = "- " + quoteObject.author;
+
 };
 
 var hideNewQuoteButton = function(){
@@ -109,6 +142,36 @@ var showNewQuoteButton = function(){
 var showAddQuoteForm = function(){
   hideNewQuoteButton();
   addQuoteForm.style.display = "block";
+
+  // THIRD ATTEMPT AT DYNAMIC PREVIEW:
+  var quoteObject = {
+    text: addQuoteText.value,
+    author: addQuoteAuthor.value
+  }
+  var quotesSection = document.querySelector("#quotes")
+  quoteArticleLive = document.createElement("article");
+  // deleteButtonLive = document.createElement("button");
+  // deleteButtonLive.id = "deleteButton";
+  // deleteButtonLive.innerText = "- delete -";
+  quoteArticleLive.classList.add("quote");
+  blockQuoteLive = document.createElement("blockquote");
+  blockQuoteLive.classList.add("blockquote-live");
+  // blockQuoteLive.innerHTML = quoteObject.text;
+  // citeLive = document.createElement("cite");
+  // citeLive.classList.add("cite-live");
+  // blockQuoteLive.appendChild(deleteButtonLive);
+  // citeLive.innerHTML = "- " + quoteObject.author;
+
+  // citeLive = document.createElement("cite");
+  // citeLive.classList.add("cite-live")
+  quotesSection.appendChild(quoteArticleLive);
+  quoteArticleLive.appendChild(blockQuoteLive);
+  // blockQuoteLive.appendChild(citeLive);
+  // citeLive.innerHTML = "fgh";
+
+  // blockQuoteLive.appendChild(citeLive);
+  // citeLive.innerHTML = "- " + quoteObject.author;
+
 }
 
 var hideAddQuoteForm = function(){
@@ -131,6 +194,71 @@ var appendQuote = function(quoteObject){
   blockQuote.appendChild(cite);
   blockQuote.appendChild(deleteButton);
   cite.innerHTML = "-" + quoteObject.author;
+
+  //messy messy messy, WET WET WET
+  var deleteButtonsCollection = document.querySelectorAll("#deleteButton");
+  var blockQuotesCollection = document.querySelectorAll("section#quotes blockquote");
+  
+  var makeDeleteQuoteFunction = function(index){
+    var deleteThisQuote = function(){
+      blockQuotesCollection[index].parentNode.removeChild(blockQuotesCollection[index]);
+    }//.bind(this)
+    return deleteThisQuote;
+  }
+
+  for (var i = 0; i < blockQuotesCollection.length; i++){
+    deleteButtonsCollection[i].onclick = makeDeleteQuoteFunction(i);
+  }
+  // try bring all the delete functionally creation out into it's own function - to run on DOMload and rerun when quoteblocks are added - commit first - I fear scope issues
+
+};//appendQuote function
+
+
+  // ### FIRST PLACE I TRIED PUTTING THE DYNAMIC PREVIEW
+// addQuoteText.onfocus = function(event){
+//   var quoteObject = {
+//     text: addQuoteText.value,
+//     author: addQuoteAuthor.value
+//   }
+//   var quotesSection = document.querySelector("#quotes")
+//   quoteArticleLive = document.createElement("article");
+//   // deleteButtonLive = document.createElement("button");
+//   // deleteButtonLive.id = "deleteButton";
+//   // deleteButtonLive.innerText = "- delete -";
+//   quoteArticleLive.classList.add("quote");
+//   blockQuoteLive = document.createElement("blockquote");
+//   blockQuoteLive.classList.add("blockquote-live");
+//   blockQuoteLive.innerHTML = quoteObject.text;
+//   citeLive = document.createElement("cite");
+//   citeLive.classList.add("cite-live");
+//   quotesSection.appendChild(quoteArticleLive);
+//   blockQuoteLive.appendChild(citeLive);
+//   quoteArticleLive.appendChild(blockQuoteLive);
+//   // blockQuoteLive.appendChild(deleteButtonLive);
+//   // citeLive.innerHTML = "- " + quoteObject.author;
+// }
+
+// addQuoteAuthor.onfocus = function(event){
+//   citeLive = document.createElement("cite");
+//   citeLive.classList.add("cite-live")
+//   blockQuoteLive.appendChild(citeLive);
+//   citeLive.innerHTML = "- " + quoteObject.author;
+
+// };
+
+addQuoteText.oninput = function(event){
+  blockQuoteLive.innerHTML = this.value;
+};
+
+addQuoteAuthor.onfocus = function(){
+  citeLive = document.createElement("cite");
+  blockQuoteLive.appendChild(citeLive);
+}
+
+addQuoteAuthor.oninput = function(event){
+
+
+  citeLive.innerHTML = "- " + this.value;
 };
 
 
@@ -141,13 +269,17 @@ addQuoteForm.onsubmit = function(event){
     author: addQuoteAuthor.value
   };
 
-  var additionalQuote = new Quote(addQuoteArgs);
-  appendQuote(additionalQuote);
 
   addQuoteText.value = "";
   addQuoteAuthor.value = "";
 
+  quoteArticleLive.parentNode.removeChild(quoteArticleLive);
+  // blockQuoteLive.parentNode.removeChild(blockQuoteLive);
+  // citeLive.parentNode.removeChild(citeLive);
+
   hideAddQuoteForm();
+  var additionalQuote = new Quote(addQuoteArgs);
+  appendQuote(additionalQuote);
 }
 
 // //////////////////////////////////////////////////
